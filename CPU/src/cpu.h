994de769004bd1cc932a9e6753921f774366236e
Tomas Jose Giampauli <tgiampauli@frba.utn.edu.ar>
2023-09-27 18:29:31 +0000

    set, sum, sub y exit, pero falta arreglar 2 warnings ┐(￣ヮ￣)┌

--- CPU/src/cpu.h
+++ CPU/src/cpu.h
@@ -37,5 +37,11 @@ t_instruccion *recibir_instruccion_memoria(int);
 void recibir_interrupcion(int);
 void devolver_a_kernel(t_contexto_ejec* contexto, op_code code, int socket_cliente);
 
+void manejar_instruccion_set(t_contexto_ejec** contexto,t_instruccion* instruccion);
+void setear_registro(t_contexto_ejec** contexto,char* registro, int valor);
+
+int obtener_valor_del_registro(char* registro_a_leer, t_contexto_ejec** contexto_actual);
+void manejar_instruccion_sub(t_contexto_ejec** contexto_actual,t_instruccion* instruccion);
+void manejar_instruccion_sum(t_contexto_ejec** contexto_actual,t_instruccion* instruccion);
 
 #endif /* CPU_H_ */
